0b7093ec05ab2f216d3998498838afc7caff349d
sonade803 <dewiresn@vcu.edu>
2026-04-26 23:31:21 -0400

    idk what im doing

--- final-personal-website/main.js
+++ final-personal-website/main.js
@@ -193,3 +193,91 @@ function newColour() {
   return ("rgb("+c[0]+", "+c[1]+", "+c[2]+")");
 }
 
+
+// document.getElementById("lol").onclick = function(){
+// var audio = document.getElementById("audio");
+// if(audio.paused) audio.onplay();
+// else audio.pause();
+// };
+
+
+// Source - https://stackoverflow.com/a/2988130
+// Posted by jAndy, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-21, License - CC BY-SA 3.0
+
+$('#lol').click(function() {
+  if ($("#song")[0].paused == false) {
+      $("#song")[0].pause();
+      $(".img1").removeClass("spin");
+  } else {
+      $("#song")[0].play();
+    $("#song")[0].volume = .75;
+    $(".img1").addClass("spin");
+
+  }
+});
+
+
+
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('change', e => {
+    if(e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
+
+
+
+
+
